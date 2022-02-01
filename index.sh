fce78a874544a69982fb8da302b5a43f66e01991
Candace J. Grimes <77850585+seagrimes@users.noreply.github.com>
2022-02-01 15:45:55 -0500

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ce#!/bin/bash
 module load bwa/0.7.17
 
-bwa index -a bwtsw -p astrotoma_ref /scratch/cjg0067/astrotoma/Op887.p_ctg.fa
+bwa index -a bwtsw -p reference_genome genome_assembly.fa
