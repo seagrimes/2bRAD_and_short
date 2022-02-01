7fa0c57d4707c7d9db8f86e476117f35170877b1
Candace J. Grimes <77850585+seagrimes@users.noreply.github.com>
2022-02-01 15:45:08 -0500

    Update bwa_mapping.sh

--- bwa_mapping.sh
+++ bwa_mapping.sh
@@ -1,9 +1,9 @@
 module load bwa/0.7.17
 module load samtools/1.11
 
-bwa mem -R "@RG\tID:Op262\tLB:Op262\tSM:Op262\tPL:ILLUMINA" -M -t 100 astrotoma_ref  /scratch/cjg0067/astrotoma/Astrotoma_agassizii_Op262_R2.fastq /scratch/cjg0067/astrotoma/Astrotoma_agassizii_Op262_R1.fastq >  /scratch/cjg0067/astrotoma/astrotoma_262.sam
-samtools flagstat  astrotoma_262.sam >  astrotoma_262.samstat.txt
-samtools view -b  astrotoma_262.sam >  astrotoma_262.bam
-samtools sort  astrotoma_262.bam -o  astrotoma_262.sorted.bam
+bwa mem -R "@RG\tID:Op262\tLB:Op262\tSM:Op262\tPL:ILLUMINA" -M -t 100 genome_ref  forward_read.fastq reverse_read.fastq >  combined.sam
+samtools flagstat  combined.sam >  combined.samstat.txt
+samtools view -b  combined.sam >  combined.bam
+samtools sort  combined.bam -o  combined.sorted.bam
 
 done
