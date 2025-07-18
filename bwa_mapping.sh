aeae1b12b96827f07c0da694a5cd79c99a47bc7a
Candace J. Grimes <77850585+seagrimes@users.noreply.github.com>
2025-07-18 16:00:05 -0400

    Update bwa_mapping.sh

--- bwa_mapping.sh
+++ bwa_mapping.sh
@@ -1,7 +1,7 @@
 module load bwa/0.7.17
 module load samtools/1.11
 
-bwa mem -R "@RG\tID:Op262\tLB:Op262\tSM:Op262\tPL:ILLUMINA" -M -t 100 genome_ref  forward_read.fastq reverse_read.fastq >  combined.sam
+bwa mem -R "@RG\tID:NAME\tLB:NAME\tSM:NAME\tPL:ILLUMINA" -M -t 100 genome_ref  forward_read.fastq reverse_read.fastq >  combined.sam
 samtools flagstat  combined.sam >  combined.samstat.txt
 samtools view -b  combined.sam >  combined.bam
 samtools sort  combined.bam -o  combined.sorted.bam
